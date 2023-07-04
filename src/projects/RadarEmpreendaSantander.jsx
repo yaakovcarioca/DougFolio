9bc6dd0d3f15714326b55ee23a7621cd3e150cd7
douglas <38635101+dougdbp@users.noreply.github.com>
2023-07-04 19:53:45 -0300

    bugfix: correção de bug nod estaque de titulo

--- src/projects/RadarEmpreendaSantander.jsx
+++ src/projects/RadarEmpreendaSantander.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import imgRadarSanta from '../assets/projectsIMG/radar-empreenda-santander.jpeg'
 
 const RadarEmpreendaSantander = () => {
   return (
@@ -14,26 +15,26 @@ const RadarEmpreendaSantander = () => {
     >
       <a href="hello">
         <img
-          class="rounded-t-lg w-full"
-          src="https://auin.unesp.br/noticias2/imagens/2021/whatsapp-image-2021-04-13-at-21-m18_u34_13042021215212_alta.jpg"
+          class="rounded-t-lg w-full h-55%"
+          src={imgRadarSanta}
           alt=""
         />
       </a>
       <div class="p-5">
-        <a href="helo">
+        <a href="#">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Radar Empreenda Santander 2020
           </h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        I developed the website for Santander Brasil, through The Bakery Brasil.
+          Project developed through The Bakery Brasil. As Frontend Developer.
         </p>
         <a
-          href="hello"
-          class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          href="#"
+          class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300"
         >
-          Read more
-          <svg
+          As Font-end Developer
+          {/* <svg
             class="ml-2 -mr-1 w-4 h-4"
             fill="currentColor"
             viewBox="0 0 20 20"
@@ -44,7 +45,7 @@ const RadarEmpreendaSantander = () => {
               d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
               clip-rule="evenodd"
             ></path>
-          </svg>
+          </svg> */}
         </a>
       </div>
     </motion.div>
